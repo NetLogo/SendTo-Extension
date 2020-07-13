045ff39a11d937e9b68fdfda859318d22d8e7b5f
LaCuneta <jeremy.baker@northwestern.edu>
2020-07-13 12:14:43 -0500

    Use the NetLogo working directory as our default directory

--- src/SendToExtension.scala
+++ src/SendToExtension.scala
@@ -4,8 +4,9 @@ import java.io.IOException
 import java.nio.charset.Charset
 import java.nio.file.{ Files, InvalidPathException, Path, Paths }
 
-import org.nlogo.api.{ Argument, Context, DefaultClassManager, ExtensionException, PrimitiveManager, Command }
+import org.nlogo.api.{ Argument, Context, DefaultClassManager, ExtensionException, PrimitiveManager, Command, Workspace => ApiWorkspace }
 import org.nlogo.core.Syntax
+import org.nlogo.nvm.{ Workspace => NvmWorkspace }
 
 class SendToExtension extends DefaultClassManager {
 
@@ -16,15 +17,24 @@ class SendToExtension extends DefaultClassManager {
   private object FilePrim extends Command {
     override def getSyntax = Syntax.commandSyntax(right = List(Syntax.StringType, Syntax.StringType))
     override def perform(args: Array[Argument], context: Context): Unit = {
-      val fileName = getPath(args(0).getString)
-      val contents = args(1).getString
-      try Files.write(fileName, contents.getBytes)
+      val nw            = checkWorkspace(context.workspace)
+      val userPath      = args(0).getString
+      val workspacePath = nw.fileManager.attachPrefix(userPath)
+      val filePath      = getPath(workspacePath)
+      val contents      = args(1).getString
+      try Files.write(filePath, contents.getBytes)
       catch {
         case ex: IOException =>
           throw new ExtensionException(s"Unable to write file: ${ex.getMessage}", ex)
       }
     }
   }
+
+  private def checkWorkspace(workspace: ApiWorkspace): NvmWorkspace =
+    workspace match {
+      case nw: NvmWorkspace => nw
+      case _                => throw new ExtensionException("We need an nvm.Workspace instance for access to the file manager.")
+    }
 
   private def getPath(path: String): Path = {
     try Paths.get(path)
